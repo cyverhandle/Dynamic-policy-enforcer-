f9822c53f7d3e846349c751b2c64f6474c81f77e
Rohit Kumar <rohitkumar150524@gmail.com>
2026-05-05 19:47:57 +0530

    Update _init_.py

---
+++ src/aggregators/_init_.py
@@ -0,0 +1,18 @@
+"""
+Threat Intelligence Aggregators Module
+Collects threat data from various OSINT sources
+"""
+
+from src.aggregators.base_aggregator import BaseAggregator
+from src.aggregators.virustotal_aggregator import VirusTotalAggregator
+from src.aggregators.alienvault_aggregator import AlienVaultAggregator
+from src.aggregators.feodo_aggregator import FeodoAggregator
+from src.aggregators.tor_aggregator import TorExitNodeAggregator
+
+__all__ = [
+    "BaseAggregator",
+    "VirusTotalAggregator",
+    "AlienVaultAggregator",
+    "FeodoAggregator",
+    "TorExitNodeAggregator"
+]
